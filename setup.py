3b9b66465193e0c685d054086d65fa1ecdbda15c
Piotr Maliński <riklaunim@gmail.com>
2017-05-28 23:15:12 +0200

    [#3] Bump version.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
                  "It show latest referrer, google queried terms or overall hits count. "
                  "It also provides optional logging and statistics for mobile devices."),
     long_description=read("README.rst"),
-    version='0.1.2',
+    version='0.2.0',
     maintainer="Piotr Malinski",
     maintainer_email="riklaunim@gmail.com",
     include_package_data=True,
